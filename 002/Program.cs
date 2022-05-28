938a9e268bbf104dc22320c0c9de4da78f0555e1
ycgzhu <ycgzhu@gmail.com>
2022-05-28 17:33:01 +0800

    002

---
+++ 002/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace _002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = new string[100];
+            try
+            {
+                using (StreamReader sr = new StreamReader ("D:/vs/workspace/001.txt"))
+                {
+                    string line;
+                    int i = 0;
+                    // 从文件读取并显示行，直到文件的末尾 
+                    while ((line = sr.ReadLine()) != null)
+                    {
+
+                        
+                             if (line.Contains("Hello world"))
+                             {
+                               line = line.Replace("Hello world", "Hello <你的名字>");
+                             }
+                        
+                        lines[i] = line;
+                        i++;
+                        
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error");
+
+            }
+
+            using (StreamWriter sw = new StreamWriter ("D:/vs/workspace/002.txt"))
+            {
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    sw.WriteLine(lines[i]);
+                }
+                sw.Close();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
